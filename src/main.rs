12d75756798165b86b576d4ad7c74683bc8efae7
Tim Waite <tim@dialexa.com>
2021-12-13 21:10:25 -0600

    Move around a block

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
+use bevy::prelude::*;
+
+struct SnakeHead;
+struct Materials {
+    head_material: Handle<ColorMaterial>,
+}
+
+fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+    // inserts the snake head material
+    commands.insert_resource(Materials {
+        head_material: materials.add(Color::rgb(0.7, 0.7, 0.7).into()),
+    });
+}
+
+fn spawn_snake(mut commands: Commands, materials: Res<Materials>) {
+    commands
+        .spawn_bundle(SpriteBundle {
+            material: materials.head_material.clone(),
+            sprite: Sprite::new(Vec2::new(10.0, 10.0)),
+            ..Default::default()
+        })
+        .insert(SnakeHead);
+}
+
+fn snake_movement(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut head_positions: Query<(&SnakeHead, &mut Transform)>,
+) {
+    for (_head, mut transform) in head_positions.iter_mut() {
+        if keyboard_input.pressed(KeyCode::Left) {
+            transform.translation.x -= 2.;
+        }
+        if keyboard_input.pressed(KeyCode::Right) {
+            transform.translation.x += 2.;
+        }
+        if keyboard_input.pressed(KeyCode::Down) {
+            transform.translation.y -= 2.;
+        }
+        if keyboard_input.pressed(KeyCode::Up) {
+            transform.translation.y += 2.;
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::build()
+        .add_startup_system(setup.system())
+        .add_startup_stage("game_setup", SystemStage::single(spawn_snake.system()))
+        .add_system(snake_movement.system())
+        .add_plugins(DefaultPlugins)
+        .run();
 }
